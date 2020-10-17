d304aa9096ad5752fb24db0c11123cb8f73e643d
decoloured <55615254+decoloured@users.noreply.github.com>
2020-10-17 19:14:40 +1100

    created entrypoint

---
+++ src/main/java/me/decoloured/chatbot/ChatBotInitializer.java
@@ -0,0 +1,10 @@
+package me.decoloured.chatbot;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChatBotInitializer implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Initalizing ChatBot");
+	}
+}
