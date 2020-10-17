d1040a6932c61020b765634539b9f548efeceff7
decoloured <55615254+decoloured@users.noreply.github.com>
2020-10-17 19:14:07 +1100

    created simple chatbot features

--- src/main/java/me/decoloured/chatbot/ChatBot.java
+++ src/main/java/me/decoloured/chatbot/ChatBot.java
@@ -1,14 +1,43 @@
 package me.decoloured.chatbot;
 
-import net.fabricmc.api.ModInitializer;
+import java.util.UUID;
 
-public class ChatBot implements ModInitializer {
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.Text;
 
-		System.out.println("Hello Fabric world!");
-	}
+public class ChatBot {
+  public final MinecraftClient client = MinecraftClient.getInstance();
+  public final PlayerEntity player = client.player;
+  private static ChatBot instance;
+
+  public static ChatBot getInstance() {
+    return instance;
+  }
+
+  public static void debug(MessageType messageType, Text message, UUID senderUuid) {
+    String command = message.getString().substring(message.getString().indexOf(">") + 2);
+    switch (command.substring(0, 2)) {
+      case "!s":
+        String[] scoreboardStrings = command.split(" ");
+        MinecraftClient.getInstance().player.sendChatMessage("/scoreboard objectives setdisplay sidebar " + scoreboardStrings[1]);
+        break;
+      case "!b":
+        MinecraftClient.getInstance().player.sendChatMessage(";" + command.substring(3));
+      case "!r":
+        MinecraftClient.getInstance().player.sendChatMessage(command.substring(3));
+      case "!l":
+      MinecraftClient.getInstance().player.sendChatMessage(String.format("XYZ: %.1f %.1f %.1f", MinecraftClient.getInstance().player.getX(), MinecraftClient.getInstance().player.getY(), MinecraftClient.getInstance().player.getZ()));
+      MinecraftClient.getInstance().player.sendChatMessage(String.format("Health: %.1f", MinecraftClient.getInstance().player.getHealth()));
+      default:
+        break;
+    }
+  }
+
+  public static void debugInfo(MessageType messageType, Text message, UUID senderUuid) {
+    System.out.println("MESSAGETYPE: " + messageType.toString());
+    System.out.println("MESSAGE: " + message.getString().substring(message.getString().indexOf(">") + 2));
+    System.out.println("SENDER: " + senderUuid.toString() + " " + message.getString().substring(1, message.getString().indexOf(">")));
+  }
 }
