489ac28026a1dd2d94928ad5aa7775e87dd34250
decoloured <55615254+decoloured@users.noreply.github.com>
2020-10-26 18:50:10 +1100

    removed health logout and added !tps command

--- src/main/java/me/decoloured/chatbot/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/decoloured/chatbot/mixin/ClientPlayNetworkHandlerMixin.java
@@ -5,26 +5,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import me.decoloured.chatbot.ChatBotConfig;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
+import me.decoloured.chatbot.ChatBot;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket;
-import net.minecraft.text.Text;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
-  private ChatBotConfig config = new ChatBotConfig();
-  private final MinecraftClient client = MinecraftClient.getInstance();
-  private float lastHealth;
-
-  @Environment(EnvType.CLIENT)
-  @Inject(method = "onHealthUpdate", at = @At("TAIL"))
-  private void onPlayerHealthUpdate(HealthUpdateS2CPacket packet, CallbackInfo ci) {
-    if (packet.getHealth() < this.lastHealth && packet.getHealth() < this.config.logoutHealth()) {
-      this.client.getNetworkHandler().getConnection().disconnect(Text.of(String.format("HealthDisconnect - %.1f health remaining", packet.getHealth())));
-    }
-    lastHealth = packet.getHealth();
+  @Inject(method = "onWorldTimeUpdate", at = @At("RETURN"))
+  private void onTimeUpdate(net.minecraft.network.packet.s2c.play.WorldTimeUpdateS2CPacket packetIn, CallbackInfo ci) {
+    ChatBot.getInstance().updateTPS(packetIn.getTime());
   }
 }
